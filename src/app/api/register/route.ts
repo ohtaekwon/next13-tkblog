a4448201f46b52d206fce229f87b10e57b81c0c3
ohtaekwon <ohtk1119@gmail.com>
2023-05-18 20:12:35 +0900

    Feat Add SIGNUP POST

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+
+import db from "@/lib/db";
+import bcrypt from "bcrypt";
+import userModel from "@/lib/models/user.model";
+
+export async function POST(req: Request) {
+  try {
+    await db.connect();
+
+    const { username, email, password: pass } = await req.json();
+
+    const isExisting = await userModel.findOne({ email });
+
+    if (isExisting) {
+      throw new Error("User already exists");
+    }
+    const hashPassword = await bcrypt.hash(pass, 10);
+
+    const newUser = await userModel.create({
+      username,
+      email,
+      password: hashPassword,
+    });
+
+    const { password, ...user } = newUser._doc; // '_doc' : the value of the user
+
+    return NextResponse.json({ user }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: "something is wrong" }, { status: 500 });
+  }
+}
