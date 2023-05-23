365e35327cffc40b1a8cf306c9be04f8bb439486
ohtaekwon <ohtk1119@gmail.com>
2023-05-23 23:30:39 +0900

    Feat PUT, DELETE  method

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -26,9 +26,45 @@ export async function PUT(req: NextRequest, ctx: any) {
   const accessToken = req.headers.get("authorization");
   const token = accessToken?.split(" ")[1];
 
-  const decodedToken = verifyJwtToken(token);
+  const decodedToken: any = verifyJwtToken(token);
 
   if (!accessToken || !decodedToken) {
     return NextResponse.json({ error: "인증이 되지 않음" }, { status: 400 });
+  }
+
+  try {
+    const body = await req.json();
+    const blog = await blogModel.findById(id).populate("authorId");
+
+    if (blog?.authorId?._id.toString() !== decodedToken._id.toString()) {
+      return NextResponse.json(
+        { msg: "Only author can update his blog" },
+        { status: 403 }
+      );
+    }
+
+    const updateBlog = await blogModel.findByIdAndUpdate(
+      id,
+      { $set: { ...body } },
+      { new: true }
+    );
+
+    return NextResponse.json(updateBlog, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+
+export async function DELETE(req: NextRequest, ctx: any) {
+  await db.connect();
+  const id = ctx.params._id;
+
+  const accessToken = req.headers.get("authorization");
+  const token = accessToken.split(" ")[1];
+
+  const decodeToken = verifyJwtToken(token);
+
+  if (!accessToken || !decodeToken) {
+    return NextResponse.json();
   }
 }
